acef7ddfbc7375e23b4f8e52808bf93fdb6e00ef
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2017-08-02 21:25:56 -0300

    Squashed 'winlamb-more/' changes from 136c358..c6d62dd
    c6d62dd Minor syntax corrections.
    
    git-subtree-dir: winlamb-more
    git-subtree-split: c6d62ddfab713c228ccbe0ce5128a1ba07a14df6

--- dictionary.h
+++ dictionary.h
@@ -14,69 +14,72 @@ namespace wl {
 template<typename keyT, typename valueT>
 class dictionary final {
 public:
-	struct entry final {
+	struct item final {
 		keyT key;
 		valueT value;
 	};
 
 private:
-	std::vector<entry> _entries;
+	std::vector<item> _items;
 
 public:
-	const       std::vector<entry>& entries() const { return this->_entries; }
-	size_t      size() const                        { return this->_entries.size(); }
-	dictionary& clear()                             { this->_entries.clear(); return *this; }
-	dictionary& reserve(size_t nEntries)            { this->_entries.reserve(nEntries); return *this; }
+	const       std::vector<item>& entries() const { return this->_items; }
+	size_t      size() const                       { return this->_items.size(); }
+	dictionary& clear()                            { this->_items.clear(); return *this; }
+	dictionary& reserve(size_t nEntries)           { this->_items.reserve(nEntries); return *this; }
 
-	entry* get(const keyT& key) {
-		for (entry& e : this->_entries) { // simple linear search
-			if (e.key == key) return &e; // non-const: entry can have its key and value modified
+	item* entry(const keyT& key) {
+		for (item& e : this->_items) { // simple linear search
+			if (e.key == key) return &e; // non-const: item can have its key and value modified
 		}
 		return nullptr;
 	}
 
-	const entry* get(const keyT& key) const {
-		for (const entry& e : this->_entries) { // simple linear search
-			if (e.key == key) return &e;
-		}
-		return nullptr;
+	const item* entry(const keyT& key) const {
+		return this->entry(key);
 	}
 
 	bool has(const keyT& key) const {
-		return this->get(key) != nullptr;
+		return this->entry(key) != nullptr;
 	}
 
 	valueT* val(const std::wstring& name) {
-		entry* e = this->get(name);
+		item* e = this->entry(name);
 		return e ? &e->value : nullptr; // directly returns the value
 	}
 
 	const valueT* val(const keyT& key) const {
-		const entry* e = this->get(key);
-		return e ? &e->value : nullptr;
+		return this->val(key);
 	}
 
-	entry& add(const keyT& key, const valueT& value = valueT()) {
-		entry* e = this->get(key);
+	item& add(const keyT& key, const valueT& value = valueT()) {
+		item* e = this->entry(key);
 		if (!e) {
-			this->_entries.emplace_back(); // only if doesn't exist yet; insert as non-ordered
-			e = &this->_entries.back();
+			this->_items.emplace_back(); // only if doesn't exist yet; insert as non-ordered
+			e = &this->_items.back();
 			e->key = key;
 		}
 		e->value = value;
-		return *e; // reference to newly added entry
+		return *e; // reference to newly added item
+	}
+
+	void remove(const keyT& key) {
+		item* it = this->entry(key);
+		if (it) {
+			this->_items.erase(this->_items.begin() + (it - &this->_items[0]));
+		}
 	}
 
 	valueT& operator[](const keyT& key) {
-		entry* e = this->get(key);
+		item* e = this->entry(key);
 		if (!e) {
-			e = &this->add(key); // if entry doesn't exist, will be added
+			e = &this->add(key); // if item doesn't exist, will be added
 		}
 		return e->value;
 	}
 };
 
-template<typename entryT> using dictionary_str = dictionary<std::wstring, entryT>;
+template<typename valueT> using dictionary_str = dictionary<std::wstring, valueT>;
 using dictionary_str_str = dictionary_str<std::wstring>;
 
 }//namespace wl
