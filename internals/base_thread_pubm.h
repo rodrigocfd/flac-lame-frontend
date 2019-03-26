35b15a1e87a4abb6b01c81a601c67944b716d92b
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2019-03-26 12:19:07 -0300

    Squashed 'winlamb/' changes from 9f5313b..7d862f8
    7d862f8 Explicit std::function argument types for base_thread methods.
    d00b7d5 Replacing _impl suffix with _pubm, to better identify base classes that implement public methods.
    6dea89f Documenting device_context classes.
    f167483 Documenting vec.h functions.
    
    git-subtree-dir: winlamb
    git-subtree-split: 7d862f85d22821d226d299a061d86af66823690a

--- internals/base_thread_pubm.h
+++ internals/base_thread_pubm.h
@@ -13,23 +13,21 @@ namespace wli {
 
 // Provides public methods for base_thread class.
 template<typename retT, retT RET_VAL>
-class base_thread_impl {
+class base_thread_pubm {
 private:
 	base_thread<retT, RET_VAL>& _baseThread;
 
 public:
-	base_thread_impl(base_thread<retT, RET_VAL>& baseThread) :
+	base_thread_pubm(base_thread<retT, RET_VAL>& baseThread) :
 		_baseThread(baseThread) { }
 
 	// Runs code asynchronously in a new detached thread.
-	template<typename funcT>
-	void run_thread_detached(funcT&& func) const noexcept {
+	void run_thread_detached(std::function<void()>&& func) const noexcept {
 		return this->_baseThread.run_thread_detached(std::move(func));
 	}
 
 	// Runs code synchronously in the UI thread.
-	template<typename funcT>
-	void run_thread_ui(funcT&& func) const noexcept {
+	void run_thread_ui(std::function<void()>&& func) const noexcept {
 		return this->_baseThread.run_thread_ui(std::move(func));
 	}
 };
