8fd44e4ca8f627d4e3463566483a6fb1117e68db
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-07-01 18:22:06 -0300

    TaskDialog prompts and F1 event.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod ids;
+mod prompt;
 mod wnd_main;
 mod wnd_run;
 
--- src/wnd_main/wnd_main_events.rs
+++ src/wnd_main/wnd_main_events.rs
@@ -1,6 +1,7 @@
-use winsafe::{co, msg};
+use winsafe::{self as w, co, msg};
 
 use crate::ids;
+use crate::prompt;
 use super::WndMain;
 
 impl WndMain {
@@ -33,8 +34,23 @@ impl WndMain {
 		});
 
 		self.wnd.on().wm_command_accel_menu(ids::MNU_ABOUT, {
+			let self2 = self.clone();
 			move || {
+				// Read version from resource.
+				let exe_name = w::HINSTANCE::NULL.GetModuleFileName().unwrap();
+				let mut res_buf = Vec::default();
+				w::GetFileVersionInfo(&exe_name, &mut res_buf).unwrap();
 
+				let fis = w::VarQueryValue(&res_buf, "\\").unwrap();
+				let fi: &w::VS_FIXEDFILEINFO = unsafe { &*(fis.as_ptr() as *const w::VS_FIXEDFILEINFO) };
+				let ver = fi.dwFileVersion();
+
+				prompt::info(self2.wnd.hwnd(), "About",
+					&format!(
+						"ID3 Padding Remover v{}.{}.{}\n\
+						Writen in Rust with WinSafe library.\n\n\
+						Rodrigo César de Freitas Dias © 2013-2021",
+						ver[0], ver[1], ver[2]));
 			}
 		});
 	}
